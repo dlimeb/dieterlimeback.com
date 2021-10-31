591291b7cec068a3b65305aaa3f3e9e8a95ba1e3
Dieter Limeback <dlimeb@gmail.com>
2021-10-31 15:29:21 -0400

    Restore vendor js but disable

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,7 @@ module.exports = function(eleventyConfig) {
   // Watch our compiled assets for changes
   eleventyConfig.addWatchTarget('./src/compiled-assets/main.css');
   eleventyConfig.addWatchTarget('./src/compiled-assets/main.js');
-  eleventyConfig.addWatchTarget('./src/compiled-assets/vendor.js');
+  // eleventyConfig.addWatchTarget('./src/compiled-assets/vendor.js');
 
   // Copy src/compiled-assets to /assets
   eleventyConfig.addPassthroughCopy({ 'src/compiled-assets': 'assets' });
